7a57fa89fd1c50a9cd477a1289b3e440e6432694
gon030203 <honguyenkhoa123@gmail.com>
2025-07-03 13:40:37 +0700

    Learning tutorial VueJS

---
+++ vue/tutorial/src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+    data() {
+        return {
+            message: "Hello World!",
+            counter: {
+                count: 0
+            },
+            text: '',
+            awesome: true
+        };
+    },
+        methods: {
+            increment() {
+                this.counter.count ++
+            },
+            toggle() {
+                this.awesome = !this.awesome
+            }
+    }
+}
+</script>
+
+<template>
+    <h1 class="message">{{ message }}</h1>
+    <br>
+  
+    <button @click="increment">Count is: {{ counter.count }}</button>
+    <br>
+  
+    <input v-model="text" placeholder="Type here">
+    <p>{{ text }}</p>
+    <br>
+  
+    <button @click="toggle">Toggle</button>
+    <br>
+  
+    <h1 v-if="awesome">Vue is awesome</h1>
+    <h1 v-else>Oh no</h1>
+</template>
+
+<style>
+.message {
+    color: red;
+}
+</style>
